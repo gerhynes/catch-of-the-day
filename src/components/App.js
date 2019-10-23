8b6916e0dfa9cb00cc669d421d292d90d6ec85ad
Gerard Hynes <gkhynes@gmail.com>
2019-10-23 21:56:42 +0100

    Load sample fishes

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import Header from "./Header";
 import Order from "./Order";
 import Inventory from "./Inventory";
+import sampleFishes from "../sample-fishes";
 
 class App extends React.Component {
   state = {
@@ -18,6 +19,9 @@ class App extends React.Component {
       fishes: fishes
     });
   };
+  loadSampleFishes = () => {
+    this.setState({ fishes: sampleFishes });
+  };
   render() {
     return (
       <div className="catch-of-the-day">
@@ -25,7 +29,10 @@ class App extends React.Component {
           <Header tagLine="Fresh Seafood Market"></Header>
         </div>
         <Order></Order>
-        <Inventory addFish={this.addFish}></Inventory>
+        <Inventory
+          addFish={this.addFish}
+          loadSampleFishes={this.loadSampleFishes}
+        ></Inventory>
       </div>
     );
   }
