d1f72db615f8144ad6d2841818f9233de9622162
Gerard Hynes <gkhynes@gmail.com>
2019-10-25 19:21:08 +0100

    Persist order state to localStorage

--- src/components/App.js
+++ src/components/App.js
@@ -13,12 +13,28 @@ class App extends React.Component {
   };
 
   componentDidMount() {
-    // Sync to Firebase
     const { params } = this.props.match;
+
+    // Reinstate localStorage
+    const localStorageRef = localStorage.getItem(params.storeId);
+
+    if (localStorageRef) {
+      this.setState({ order: JSON.parse(localStorageRef) });
+    }
+
+    // Sync to Firebase
     this.ref = base.syncState(`${params.storeId}/fishes`, {
       context: this,
       state: "fishes"
     });
+  }
+
+  componentDidUpdate() {
+    console.log(this.state.order);
+    localStorage.setItem(
+      this.props.match.params.storeId,
+      JSON.stringify(this.state.order)
+    );
   }
 
   componentWillUnmount() {
